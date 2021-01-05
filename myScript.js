18a480b6a7578d7e5921674ab287bcbfe01bc133
TBSDrJ <marcus.jaiclin@bishops.com>
2021-01-04 22:43:53 -0800

    Change initial view to have input text boxes and then capture those.

--- myScript.js
+++ myScript.js
@@ -1,8 +1,21 @@
+// Just check to see that we found the div element we are looking for
 myDiv = document.getElementById("myDiv");
 console.log(myDiv);
+
+// Click event to attach to button
 function myClick () {
+  // Quick check to verify that the function executes.
   console.log("test function");
+  // Get the values that were input into the two text boxes.
+  var fname = document.getElementById('fname').value;
+  var lname = document.getElementById('lname').value;
+  console.log(fname, lname);
+  // By using = we *replace* the entire contents of the div tag.
   myDiv.innerHTML = "\n";
-  myDiv.innerHTML = "\t\t<h1>Whoa.</h1>\n"
+  // Now, using += we are *appending* to the new contents of the div tag.
+  myDiv.innerHTML += "\t\t<h1>Whoa.</h1>\n"
+  // Notice mixing of quotation marks, just like in Python.
   myDiv.innerHTML += "\t\t<img src='default.png' />\n";
+  // Notice here that we are appending the values of the variables.
+  myDiv.innerHTML += "\t\t<p>" + fname + " " + lname + "</p>\n";
 }
